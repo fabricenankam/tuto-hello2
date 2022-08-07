ec7120056897524fe4238fd1218c57e7be9df1bf
fabrice <fanas009@yahoo.fr>
2022-08-07 09:24:21 +0100

    autre modif message hello

--- src/metier/Hello.java
+++ src/metier/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 	}
 	
 	public String afficherHello() {
-		return "Hello World and welcome to my TV chanel.com !";
+		return "Hello World and welcome to my TV chanel !";
 		
 	}
 		
